3121a533075128c890b099b003483c75afacf39c
James Henager <jameschenager@gmail.com>
2020-06-19 13:04:16 -0700

    add function to increase the pizza object's topping value to reflect checkboxes

--- js/scripts.js
+++ js/scripts.js
@@ -13,12 +13,39 @@ Pizza.prototype.price = function () {
 // User Interface Logic
 
 
+
 $(document).ready(function(){
-  let pizza = new Pizza();
-  const beverage = $("#pizzaSize").val();
+  let pizza = new Pizza(0,0);
+  const pizzaSize = $("#pizzaSize").val();
+  let toppings = [];
+  let toppingsValue;
+  
 
 
-});
+  $('input[type="checkbox"]').click(function(){
+    if($(this).is(":checked")){
+      pizza.toppings +=1
+      toppings.push($(this).val());
+      console.log(pizza.toppings);
+      // return toppings;
+    }
+    else if($(this).is(":not(:checked)")){
+        console.log("Checkbox is unchecked.");
+    }
+    });
+    
+  $("button#toppingAdd").click(function(){
+    toppings.forEach(parseInt);
+    console.log(toppings);
+    return;
+  });
+   
+    
+  });
+
+
+
+
 
 
 
