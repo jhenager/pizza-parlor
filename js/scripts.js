e1e374b458003554e051da624239883af532766a
James Henager <jameschenager@gmail.com>
2020-06-19 16:02:18 -0700

    clean up the presentation of the code

--- js/scripts.js
+++ js/scripts.js
@@ -34,32 +34,21 @@ function pizzaPrice (pizza) {
 }
 
 $(document).ready(function(){
-
   let pizza1 = new Pizza(0,0);
-  
-  
   console.log(pizza1.size);
-
-
-
   $('input[type="checkbox"]').click(function(){
     if($(this).is(":checked")){
-      pizza1.toppings +=1
-  
+      pizza1.toppings +=1;
       console.log(pizza1.toppings);
     }
     else if($(this).is(":not(:checked)")){
         console.log("Checkbox is unchecked.");
-    }
+      }
     });
-    
   $("button#toppingAdd").click(function(){
-      // pizza.size = ($("#pizzaSize").val());
       pizzaCalculator(pizza1);
       pizzaSizeConverter(pizza1);
-      console.log(pizza1.size)
       pizzaPrice(pizza1);
-      console.log(pizza1);
       $(".total").text(" " + "$" + pizza1.toppings);
 
   });
