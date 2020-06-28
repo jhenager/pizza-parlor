50b30c478ec9a3c0247ea5cc26d8346b4e816c02
James Henager <jameschenager@gmail.com>
2020-06-28 12:36:05 -0700

    add working function to add values in toppings array

--- js/scripts.js
+++ js/scripts.js
@@ -10,26 +10,29 @@ Pizza.prototype.price = function () {
   this.totalPrice = this.toppings + this.size;
 }
 
+
 // User Interface Logic
 
 
 $(document).ready(function(){
   let pizza1 = new Pizza(0,0);
-  $('input[type="checkbox"]').click(function(){
-    if($(this).is(":checked")){
-      pizza1.toppings +=1;
-      console.log(pizza1.toppings);
-    }
-    else if($(this).is(":not(:checked)")){
-        console.log("Checkbox is unchecked.");
-      }
-    });
+  
   $("#toppingAdd").submit(function(){
     event.preventDefault();
     pizza1.size = parseInt($("#pizzaSize").val());
+    let toppings = [];
+    $('input[type="checkbox"]:checked').each(function() {
+      toppings.push(parseInt(this.value));
+    });
+    function getSum(total, num) {
+      return total + Math.round(num);
+    }
+    function addToppings(item) {
+      pizza1.toppings = toppings.reduce(getSum, 0);
+    } 
+    addToppings();
     pizza1.price();
     $(".total").text(" " + "$" + pizza1.totalPrice);
-    $("button#toppingAdd").hide();
   }); 
 });
 
